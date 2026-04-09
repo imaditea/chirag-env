7d82fb2f25e96d87df3f4388c75693c570896968
Aditi Singh <iaditisinghh@gmail.com>
2026-04-09 07:35:29 +0530

    Delete graders/graders/hard_grader.py

--- graders/graders/hard_grader.py
+++
@@ -1,7 +0,0 @@
-def grade(obs, action, reward, info):
-    if info.get("difficulty") == 3:
-        if action == 3 and info.get("success"):
-            return 1.0
-        else:
-            return 0.0
-    return reward / 10.0
