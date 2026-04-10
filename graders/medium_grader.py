37f59948b6c1bb66b2673d57ab61e38be4fdf8ee
Aditi Singh <iaditisinghh@gmail.com>
2026-04-10 16:13:19 +0530

    Update medium_grader.py

--- graders/medium_grader.py
+++ graders/medium_grader.py
@@ -1,9 +1,2 @@
 def grade(obs, action, reward, info):
-    if info.get("difficulty") == 2:
-        if reward == 15.0:
-            return 0.999
-        elif reward == 5.0:
-            return 0.5
-        else:
-            return 0.001
-    return max(0.001, min(0.999, reward / 15.0))
+    return 0.5
