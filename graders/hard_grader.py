318f5535f8be91ab48f218179a9b81c9aedbcd65
Aditi Singh <iaditisinghh@gmail.com>
2026-04-10 16:12:52 +0530

    Update hard_grader.py

--- graders/hard_grader.py
+++ graders/hard_grader.py
@@ -1,7 +1,2 @@
-def grade(obs, action, reward, info):
-    if info.get("difficulty") == 3:
-        if action == 3 and info.get("success"):
-            return 0.999
-        else:
-            return 0.001
-    return max(0.001, min(0.999, reward / 10.0))
+ def grade(obs, action, reward, info):
+    return 0.5
